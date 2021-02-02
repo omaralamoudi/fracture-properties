57b7f37fa227b109efab9904a948d3aa518e7e3a
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-02-02 15:09:50 -0600

    renamed the manuscript files

--- scripts/main.m
+++ scripts/main.m
@@ -1,1 +1,15 @@
 %% This is the entry way into the rest of the scripts
+
+%% Go to primary directory
+
+if (ispc)
+    cd 'C:\Users\oma385\Dropbox\GraduateSchool\PhD\Projects\';
+elseif (ismac)
+    cd '~/Dropbox/GraduateSchool/PhD/Projects/Fracture Detection and Property Measurements/scripts';    
+end
+addpath(genpath('.'));
+
+home;
+InitColormaps();
+
+%% 
