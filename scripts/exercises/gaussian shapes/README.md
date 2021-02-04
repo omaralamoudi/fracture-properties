7d708ccc05b9fe2992d30f5f7cc859c86ead6ffb
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-02-04 13:05:11 -0600

    Added a directory to discuss Gaussian functions of different dimentions

---
+++ scripts/exercises/gaussian shapes/README.md
@@ -0,0 +1,2 @@
+# Details about the functions used in the script `ShapesOfGaussians.m`
+This script shows how the Gaussian function looks in different dimentions
---
+++ scripts/exercises/gaussian shapes/ShapesOfGaussians.m
@@ -0,0 +1,4 @@
+%% The shapes of the Gaussian function in different dimentions
+%  Here I want to explain the potential of used different types of gaussian
+%  functions in detecting the fractures in microCT images. 
+
