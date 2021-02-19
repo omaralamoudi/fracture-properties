8c65485582cef6ee0771f5b820cfc275d4ef0797
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-02-19 16:43:10 -0600

    removed the deleted files
    Former-commit-id: d2f990223f9640c9797ff64236eebc8a899b630a

--- scripts/functions/sortEigenValues.m
+++
@@ -1,6 +0,0 @@
-function sortedEigenValues = sortEigenValues(EigenMatrix)
-    for n = 1:size(EigenMatrix,1)
-        sortedEigenValues(n) = EigenMatrix(n,n);
-    end
-    sortedEigenValues = sort(sortedEigenValues);
-end
