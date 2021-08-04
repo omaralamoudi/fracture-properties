46a0553620a4fc26c412e4a789d74a947c36b5f1
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-08-04 12:27:06 +0300

    minor edit
    Former-commit-id: 4a6fc56e868d0f000ca27f644d829ba701a8939c

--- scripts/matlab/functions/mshff.m
+++ scripts/matlab/functions/mshff.m
@@ -1,10 +1,32 @@
-function result = mshff(inputimage,s)
+function results = mshff(inputimage,s)
 %MSHFF impelements Voorn et al. (2013) algorith for multi-scale Hessian
 %fracture filtering.
-%
+%   MSHFF(image, s) computes an enhanced image by convolving different
+%   hessian kernals corresponding to the scaling parameter s. s is either a
+%   scalar or a vector with different scales measured in voxels. E.g.,
+%   result = MSHFF(image, [1 2 6]) produces an image after enhancing
+%   features that are 1, 2, and 6 voxels in scale.
+
 % By Omar Alamoudi: omar.alamoudi@gmail.com
 % Last updated:     April 6, 2021
 %
+if is2d(inputimage)
+    kernels = get_kernals(s,9);
+elseif is3D(inputimage)
+else
+    error('Image is neither 2D or 3D'); 
+end
 
 
+
+end
+
+function itis = is2d(img)
+    dim  = size(size(img));
+    itis = dim(2) == 2;
+end
+
+function itis = is3d(img)
+    dim  = size(size(img));
+    itis = dim(2) == 3;
 end
