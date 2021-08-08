c52668505f3c33930b3d2cab3de8b2c8956a428d
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-08-08 11:10:22 +0300

    simple function name edit
    Former-commit-id: 7a21c03c58f818bb1950af7a9a846bf04e3797c3

--- scripts/matlab/functions/writeImageSeq.m
+++ scripts/matlab/functions/writeImageSeq.m
@@ -1,5 +1,5 @@
 %WRITEIMAGES helper function that writes images slices from a 3D array.
-function writeimageseq(image,directory,imagename,extention)
+function writeImageSeq(image,directory,imagename,extention)
     for i = 1:size(image,3)
         imwrite(image(:,:,i),[directory,filesep,imagename,'_',num2str(i),extention]); 
     end
