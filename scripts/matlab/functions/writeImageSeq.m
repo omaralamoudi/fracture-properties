1abab304de08548a61987e22b776dbf386487a0f
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-08-09 10:52:42 +0300

    create the directory if it doesn't exist
    Former-commit-id: 79c442832996733db60059ed48de5443d3cc9c7d

--- scripts/matlab/functions/writeImageSeq.m
+++ scripts/matlab/functions/writeImageSeq.m
@@ -1,5 +1,9 @@
 %WRITEIMAGES helper function that writes images slices from a 3D array.
 function writeImageSeq(image,directory,imagename,extention)
+% create the target directory
+if not(exist(directory,'dir')), mkdir(directory); end
+addpath(genpath(directory));
+
     for i = 1:size(image,3)
         imwrite(image(:,:,i),[directory,filesep,imagename,'_',num2str(i),extention]); 
     end
