6d392860c99591f355c9de4aece5f6fbf819a144
Omar Alamoudi <omar.alamoudi@gmail.com>
2021-08-18 15:42:53 -0500

    minor edits
    Former-commit-id: bae8d2fe8596c31d8455f3b7235335af579dbb33

--- scripts/matlab/functions/mshff_prototype.m
+++ scripts/matlab/functions/mshff_prototype.m
@@ -4,14 +4,16 @@ data2 = loadImageSeq('output/3D/synth images/blurred+noisy','.tif');
 tt = tic;
 s = fracAps/2;
 cumresult = zeros(size(data2.image));
+gamma = 0.7;
 for i = 1:length(s)
     svalue = ones(1,3) * s(i);
-    result(i) = mshff(data2.image,svalue);
-    cumresult = cumresult + result(i).Cs.image; 
+    mshffResult(i) = mshff(data2.image,svalue,gamma);
+    cumresult = cumresult + mshffResult(i).Cs.image; 
 end
 cumresult_norm = cumresult / max(cumresult(:));
 TT = toc(tt);
 %%
+figure
 subplot(1,2,1);
 imagesc(data2.image(:,:,2));
 colorbar
