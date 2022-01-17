d435ca0d8336b8075dc655ec1fa075fcaf897ff2
Omar Alamoudi <omar.alamoudi@gmail.com>
2022-01-17 12:56:39 -0600

    Using linear indexing
    I will compare using linear indexing with the prior approach. The reason is that linear indexing might reduce the need to use two different loops for 2d and 3d images, it also might yield a faster output. The file inside scripts/matlab/runs/comparing mshff to mshff_prototype will be used for comparing improved implementations of mshff

---
+++ scripts/matlab/runs/comparing mshff to mshff_prototype/comparing_mshff_to_mshff_prototype.m
@@ -0,0 +1,72 @@
+% this script will serve as a comparison point for adding features or
+% improving mshff. The basic approach is to add modifications to
+% mshff_prototype.m and compare the performance with mshff.m. If the
+% prototype is more performant, we adopt the change
+
+clearvars;
+data(1).path.pc = 'C:\Users\oma385\Dropbox\GraduateSchool\PhD\Projects\fracture properties\using Voorn-2013\runs\test run 01\root\input';
+data(1).path.mac= '~/Dropbox/GraduateSchool/PhD/Projects/fracture properties/using Voorn-2013/runs/test run 01/root/input';
+data(2).path.pc = 'C:/Users/oma385/Dropbox/GraduateSchool/PhD/Projects/fracture properties/scripts/matlab/output/3D/synth images/blurred+noisy';
+data(2).path.mac= '~/Dropbox/GraduateSchool/PhD/Projects/fracture properties/scripts/matlab/output/3D/synth images/blurred+noisy';
+
+i = 2;
+data2 = loadImageSeq(data(i).path.pc,'.tif');
+% data4 = uiLoadImageSeq('.tif');
+% inputimage = data4.image(:,:,5:20);
+inputimage = data2.image;
+
+s = 1:2:7;
+fracAps = 2*s;
+gamma = 0.7;
+
+%% mshff_prototype
+tt = tic;
+s = fracAps/2;
+mshff_prototypeCumResult = zeros(size(inputimage));
+for i = 1:length(s)
+    svalue = ones(1,ndims(inputimage)) * s(i);
+    mshff_prototypeResult(i) = mshff_prototype(inputimage,svalue,gamma);
+    mshff_prototypeCumResult = mshff_prototypeCumResult + mshff_prototypeResult(i).Cs.image; 
+end
+mshff_prototypeCumResult_norm = mshff_prototypeCumResult / max(mshff_prototypeCumResult(:));
+TT = toc(tt);
+disp(['mshff finished in: ',num2str(TT),' secs']);
+
+%%
+tt = tic;
+s = fracAps/2;
+mshffCumResult = zeros(size(inputimage));
+for i = 1:length(s)
+    svalue = ones(1,ndims(inputimage)) * s(i);
+    mshffResult(i) = mshff(inputimage,svalue,gamma);
+    mshffCumResult = mshffCumResult + mshffResult(i).Cs.image; 
+end
+mshffCumResult_norm = mshffCumResult / max(mshffCumResult(:));
+TT = toc(tt);
+disp(['mshff finished in: ',num2str(TT),' secs']);
+
+%%
+if ndims(inputimage) == 3
+    zslice = ceil(size(inputimage,3)/2);
+else
+    zslice = 1;    
+end
+
+figure
+subplot(2,2,1);
+imagesc(inputimage(:,:,zslice));
+colorbar
+subplot(2,2,2);
+imagesc(1-mshff_prototypeCumResult_norm(:,:,zslice));
+title('FracMap');
+colormap gray
+colorbar
+
+subplot(2,2,3);
+imagesc(inputimage(:,:,zslice));
+colorbar
+subplot(2,2,4);
+imagesc(1-mshffCumResult_norm(:,:,zslice));
+title('mshff');
+colormap gray
+colorbar
