48412054f4c3b20eeaeb60f821fdb88980af6534
Omar Alamoudi <omar.alamoudi@gmail.com>
2022-01-17 18:02:43 -0600

    added some comments for clarity

--- scripts/matlab/functions/sortEigenValues.m
+++ scripts/matlab/functions/sortEigenValues.m
@@ -1,4 +1,6 @@
 function sortedEigenValues = sortEigenValues(EigenMatrix)
+% extract diagonal elements into a row vector/array
 sortedEigenValues = diag(EigenMatrix)';
+% sort array elements from smallest to largest
 sortedEigenValues = sort(sortedEigenValues);
 end
