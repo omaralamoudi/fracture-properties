47ca4771e4ea06f5498b5bc4b67f1d957b99d9ff
Omar Alamoudi <omar.alamoudi@gmail.com>
2022-02-11 13:24:57 -0600

    Updated getHessianKernels comparison script

--- scripts/matlab/functions/getHessianKernels_compared_to_getHessianKernels_prototype.m
+++ scripts/matlab/functions/getHessianKernels_compared_to_getHessianKernels_prototype.m
@@ -9,24 +9,33 @@
 close all; clearvars;
 
 sx = 3;
-sy = 5;
-sz = 9;
+sy = 9;
+sz = 5;
 s  = [sx, sy, sz];
 % s = [3 3 3];
 % s  = [sx, sy];
-kernel_multiplier = 9;
-k = getHessianKernels(s);
-kp = getHessianKernels_prototype(s);
+kernel_multiplier = 5;
 
-linkedAxes3dO = plotKernels(k,'original');
-linkedAxes3dP = plotKernels(kp,'protytpe');
+% k = getHessianKernels(s);
+k =             getHessianKernels(s,numel(s),kernel_multiplier,1);
+kp  = getHessianKernels_prototype(s,numel(s),kernel_multiplier,1);
+kp2 = getHessianKernels_prototype(s,numel(s),kernel_multiplier,2);
 
-function linkedAxes3d = plotKernels(kernels,supertitle)
+linkedAxes3dO = plotKernels(k,'component','original');
+linkedAxes3dP = plotKernels(kp,'component','protytpe');
+linkedAxes3dP2 = plotKernels(kp2,'component','protytpe finite def');
+
+
+function linkedAxes3d = plotKernels(kernels,element,supertitle)
 if nargin < 2
     supertitle = '';
 end
 screenSize = get(0,'ScreenSize');
-clims = [min(kernels.values(:)) max(kernels.values(:))]*.5;
+if contains(element,'norm')
+    clims = [0 1];
+else
+    clims = [min(kernels.values(:)) max(kernels.values(:))]*.5;
+end
 if kernels.dims == 2
     fig = figure('Name','2D example','Position',[screenSize(3)*.05 screenSize(4)*.05 screenSize(3)*.50 screenSize(4)*.85]); %#ok<NASGU>
     t = tiledlayout(kernels.dims,kernels.dims);
@@ -37,10 +46,10 @@ if kernels.dims == 2
     for i = 1:kernels.dims^2
         ax(i) = nexttile;
 %         clims = [kernels(i).min kernels.max]*.5; % this is incorrect
-        imagesc(kernels.values(:,:,i),clims);
-        ax(i).XTick = (1:size(kernels.values(:,:,i),2)+1)-1/2;
+        imagesc(kernels.(element){i},clims);
+        ax(i).XTick = (1:size(kernels.(element){i},2)+1)-1/2;
         ax(i).XTickLabel = {};
-        ax(i).YTick = (1:size(kernels.values(:,:,i),1)+1)-1/2;
+        ax(i).YTick = (1:size(kernels.(element){i},1)+1)-1/2;
         ax(i).YTickLabel = {};
         ax(i).TickDir = 'out';
         ax(i).Title.String = kernels.component_order{i};
@@ -51,7 +60,7 @@ if kernels.dims == 2
     end
     linkaxes(ax);
     linkedAxes3d = gobjects();
-    if ~strcmp(supertitle,''), suptitle(supertitle);end
+    if ~strcmp(supertitle,''), fig.Name = [supertitle,', ',element]; end
 elseif kernels.dims == 3
     % 2D slice
     fig3 = figure('Position',[screenSize(3)*.05 screenSize(4)*.05 screenSize(3)*.50 screenSize(4)*.85]); %#ok<NASGU>
@@ -63,10 +72,10 @@ elseif kernels.dims == 3
     for i = 1:kernels.dims^2
         axSlices(i) = nexttile;
         zslice = ceil(kernels.nz/3);
-        imagesc(kernels.values(:,:,zslice,i),clims); hold on
-        axSlices(i).XTick = (1:size(kernels.values(:,:,i),2)+1)-1/2;
+        imagesc(kernels.(element){i}(:,:,zslice),clims); hold on
+        axSlices(i).XTick = (1:size(kernels.(element){i},2)+1)-1/2;
         axSlices(i).XTickLabel = {};
-        axSlices(i).YTick = (1:size(kernels.values(:,:,i),1)+1)-1/2;
+        axSlices(i).YTick = (1:size(kernels.(element){i},1)+1)-1/2;
         axSlices(i).YTickLabel = {};
         axSlices(i).TickDir = 'out';
         axSlices(i).Title.String = kernels.component_order{i};
@@ -76,19 +85,18 @@ elseif kernels.dims == 3
         c(i).Location = 'southoutside';
     end
     linkaxes(axSlices);
-    if ~strcmp(supertitle,''), suptitle(supertitle);end
+    if ~strcmp(supertitle,''), fig3.Name = [supertitle,', ',element]; end
     % 3d shapes
     fig2 = figure('Position',[screenSize(3)*.05 screenSize(4)*.05 screenSize(3)*.50 screenSize(4)*.85]); %#ok<NASGU>
     t = tiledlayout(kernels.dims,kernels.dims);
     t.TileSpacing   = 'compact';
     t.Padding       = 'compact';
     ax3d = gobjects(kernels.dims^2,1); % initializing graphics object
-    val = mean([min(kernels.values(:)) max(kernels.values(:))])*1.25; % mean value
     for i = 1:kernels.dims^2
         ax3d(i) = nexttile;
-%         val = mean([kernels.min kernels.max]); % mean value
-        p = patch(isosurface(kernels.values(:,:,:,i),val));
-        p.FaceColor = 'blue';
+        val = (max(kernels.(element){i}(:)) - min(kernels.(element){i}(:)))/2; % mean value for each component. This is to inspect the shape not to compare magnitudes
+        p = patch(isosurface(kernels.(element){i},val));
+        p.FaceColor = 'yellow';
         p.FaceAlpha = .3;
         p.EdgeColor = 'none';
         daspect([1 1 1])
@@ -98,15 +106,15 @@ elseif kernels.dims == 3
         xslice = ceil(kernels.nx * (1/2));
         yslice = ceil(kernels.ny * (1/2));
         zslice = ceil(kernels.nz * (1/2));
-        sslice = slice(kernels.values(:,:,:,i),xslice,yslice,zslice);
+        sslice = slice(kernels.(element){i}(:,:,:),xslice,yslice,zslice);
         set(sslice,'FaceAlpha',1,'FaceColor','flat','EdgeAlpha',0);
         sslice(end).FaceAlpha = 1;
         xlabel('x'); ylabel('y'); zlabel('z');
         ax3d(i).XLim = [1 kernels.nx];
-        ax3d(i).XTick = (1:size(kernels.values(:,:,i),2)+1)-1/2;
+        ax3d(i).XTick = (1:size(kernels.(element){i},2)+1)-1/2;
         ax3d(i).XTickLabel = {};
         ax3d(i).YLim = [1 kernels.ny];
-        ax3d(i).YTick = (1:size(kernels.values(:,:,i),1)+1)-1/2;
+        ax3d(i).YTick = (1:size(kernels.(element){i},1)+1)-1/2;
         ax3d(i).YTickLabel = {};
         ax3d(i).TickDir = 'out';
         ax3d(i).Title.String = kernels.component_order{i};
@@ -121,7 +129,7 @@ elseif kernels.dims == 3
         axis equal tight
     end
     linkedAxes3d = linkprop(ax3d,'View'); % I need the handle to the linked properties for this to work as expected. See: https://www.mathworks.com/matlabcentral/answers/408254-linkprop-not-persisting-after-changing-scope
-    if ~strcmp(supertitle,''), suptitle(supertitle);end
+    if ~strcmp(supertitle,''), fig2.Name = [supertitle,', ',element];end
     rotate3d on
 end
 end
