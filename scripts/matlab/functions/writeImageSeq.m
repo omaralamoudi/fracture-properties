04e974a6bb0c67bc7093ea7379139c39a5c2ba8d
Omar Alamoudi <omar.alamoudi@gmail.com>
2022-03-01 17:48:36 -0600

    fixed loadImageSeq
    Previously, when removing an item, the list of items are shifted upward by one, so the item that shifted to the position i is not evaluated since i increments by 1, but not it is evaluated becasue i is decremented when removing item from list

--- scripts/matlab/functions/writeImageSeq.m
+++ scripts/matlab/functions/writeImageSeq.m
@@ -5,6 +5,6 @@ if not(exist(directory,'dir')), mkdir(directory); end
 addpath(genpath(directory));
 
     for i = 1:size(image,3)
-        imwrite(image(:,:,i),[directory,filesep,imagename,'_',num2str(i),extention]); 
+        imwrite(image(:,:,i),fullfile(directory,[imagename,'_',num2str(i),'.',extention])); 
     end
 end
